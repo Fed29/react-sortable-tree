84c9acc8b10ab7a3c625293f522a7a535d790f77
Federico Angaramo <angaramo@ismb.it>
2016-10-24 22:51:26 +0200

    Revert "Revert "Add constraint: DnD only at the same depth""
    This reverts commit be2fc1b6b70918c59e92c96d01411d2d1ddec691.

--- src/react-sortable-tree.js
+++ src/react-sortable-tree.js
@@ -206,7 +206,7 @@ class ReactSortableTree extends Component {
         });
     }
 
-    dragHover({ node: draggedNode, depth, minimumTreeIndex }) {
+    dragHover({ node: draggedNode, path, depth, minimumTreeIndex }) {
         const addedResult = insertNode({
             treeData: this.state.draggingTreeData,
             newNode: draggedNode,
@@ -221,6 +221,12 @@ class ReactSortableTree extends Component {
         const swapFrom   = addedResult.treeIndex;
         const swapTo     = minimumTreeIndex;
         const swapLength = 1 + getDescendantCount({ node: draggedNode });
+
+        if(path.length !== expandedParentPath.length)
+            return;
+
+        console.log(path, depth, minimumTreeIndex, addedResult);
+
         this.setState({
             rows: swapRows(rows, swapFrom, swapTo, swapLength),
             swapFrom,
@@ -236,7 +242,9 @@ class ReactSortableTree extends Component {
     }
 
     endDrag(dropResult) {
-        if (!dropResult) {
+        console.log("END DRAG", dropResult);
+
+        if (!dropResult || dropResult.depth !== dropResult.path.length-1) {
             return this.setState({
                 draggingTreeData: null,
                 swapFrom: null,
